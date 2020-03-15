869a0a0c3c02064cd0a475d57b53775bfee8ffc2
banaanihillo <banaanihillo@protonmail.ch>
2020-03-15 16:08:24 +0200

    Commited exercise1-4

---
+++ exercise1-4/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Part = (props) => {
+    return (
+        <div>
+            <p> {props.name} contains {props.exercises} exercises. </p>
+        </div>
+    )    
+}
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1> Description of the course: {props.course} </h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part name = {props.parts[0].name} exercises = {props.parts[0].exercises} />
+            <Part name = {props.parts[1].name} exercises = {props.parts[1].exercises} />
+            <Part name = {props.parts[2].name} exercises = {props.parts[2].exercises} />
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            Total number of exercises: {props.parts[0].exercises
+            + props.parts[1].exercises
+            + props.parts[2].exercises }
+        </div>
+    )
+}
+
+const App = () => {
+    const course = "Stack Half-Full"
+    const parts = [
+        {
+            name: "Fundamentals of React",
+            exercises: 10
+        },
+        {
+            name: "Using props to pass data",
+            exercises: 7
+        },
+        {
+            name: "State of a component",
+            exercises: 14
+        }
+    ]
+
+    return (
+        <div>
+            <Header course = {course} />
+            <Content parts = {parts} />
+            <Total parts = {parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
