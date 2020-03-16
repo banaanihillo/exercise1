6041559989073f51d2adb39bf1f96edc4f30a820
banaanihillo <banaanihillo@protonmail.ch>
2020-03-16 12:11:12 +0200

    Commited exercise1-8 (Unicafe)

---
+++ exercise1-8-unicafe/src/index.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const Statistics = ({great, neutral, notGreat }) => {
+    return (
+        <div>
+            <h1> Summary of feedback </h1>
+            <div>
+                <p> Positive feedback given: {great} </p>
+                <p> Neutral feedback given: {neutral} </p>
+                <p> Constructive criticism given: {notGreat}</p>
+                <p> Amount of feedback given: {great + neutral + notGreat}</p>
+                <p> Average score: {(great - notGreat) / (great + neutral + notGreat)}</p>
+                <p> Positive: {(great / (great + neutral + notGreat) ) * 100} % </p>
+            </div>
+        </div>
+    )
+}
+
+const App = () => {
+    const [great, setGreat] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [notGreat, setNotGreat] = useState(0)
+
+    return (
+        <div>
+            <h1> How did Unicafe do today? </h1>
+            <div>
+                <button onClick = {() => setGreat(great + 1)}>
+                    Great!
+                </button>
+                <button onClick = {() => setNeutral(neutral + 1)}>
+                    Eh, I dunno
+                </button>
+                <button onClick = {() => setNotGreat(notGreat + 1)}>
+                    Not so great
+                </button>
+            </div>
+
+            <Statistics great = {great} neutral = {neutral} notGreat = {notGreat} />
+
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
